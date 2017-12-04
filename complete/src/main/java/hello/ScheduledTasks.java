77f53a99b966937d8509d2c58a97d031a11092a4
195920 <Mar54321>
2017-12-04 11:27:04 -0500

    Configuring Schedular pool

--- complete/src/main/java/hello/ScheduledTasks.java
+++ complete/src/main/java/hello/ScheduledTasks.java
@@ -2,6 +2,7 @@ package hello;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,36 +16,53 @@ public class ScheduledTasks {
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
+    private AtomicInteger counter = new AtomicInteger(0);
+
     //@Scheduled(fixedRate = 5000)
     @Scheduled(fixedDelayString = "${fixedDelay.in.milliseconds}")
     public void reportCurrentTime() {
-        logger.info("The time is now {}", dateFormat.format(new Date()));
+
+        int jobId = counter.incrementAndGet();
+
+        logger.info("The time is now {}", dateFormat.format(new Date())+ ", jobId: " + jobId);
         logger.info("ReportCurrentTime Current Thread : {}", Thread.currentThread().getName());
     }
 
     @Scheduled(fixedRateString = "${fixedRate.in.milliseconds}")
     public void scheduleTaskWithFixedRate() {
-        logger.info("Fixed Rate Task :: Execution Time - {}", dateFormat.format(new Date()) );
+
+        int jobId = counter.incrementAndGet();
+
+        logger.info("Fixed Rate Task :: Execution Time - {}", dateFormat.format(new Date())+ ", jobId: " + jobId);
         logger.info("Fixed Rate Task Current Thread : {}", Thread.currentThread().getName());
     }
 
     @Scheduled(fixedDelayString = "${fixedDelay.in.milliseconds}")
     public void scheduleTaskWithFixedDelay() {
-        logger.info("Fixed Delay Task :: Execution Time - {}", dateFormat.format(new Date()) );
+
+        int jobId = counter.incrementAndGet();
+
+        logger.info("Fixed Delay Task :: Execution Time - {}", dateFormat.format(new Date()) + ", jobId: " + jobId);
         logger.info("Fixed Delay Task Current Thread : {}", Thread.currentThread().getName());
     }
 
 
     @Scheduled(fixedDelayString = "${fixedRate.in.milliseconds}",initialDelayString = "${intialDelay.in.milliseconds}")
     public void scheduleTaskWithInitialDelay() {
-        logger.info("Intial Delay Task :: Execution Time - {}", dateFormat.format(new Date()) );
+
+        int jobId = counter.incrementAndGet();
+
+        logger.info("Intial Delay Task :: Execution Time - {}", dateFormat.format(new Date()) + ", jobId: " + jobId);
         logger.info("Intial Delay Task Current Thread : {}", Thread.currentThread().getName());
 
     }
 
     @Scheduled(cron = "${cron.expression}")
     public void scheduleTaskWithCronExpression() {
-        logger.info("Cron Task :: Execution Time - {}", dateFormat.format(new Date()) );
+
+        int jobId = counter.incrementAndGet();
+
+        logger.info("Cron Task :: Execution Time - {}", dateFormat.format(new Date())+ ", jobId: " + jobId);
         logger.info("Cron Task Current Thread : {}", Thread.currentThread().getName());
     }
 }
--- complete/src/main/java/hello/config/SchedulerConfig.java
+++ complete/src/main/java/hello/config/SchedulerConfig.java
@@ -1,22 +1,39 @@
 package hello.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.IntervalTask;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.Date;
 
 @Configuration
 public class SchedulerConfig implements SchedulingConfigurer {
+
     private final int POOL_SIZE = 10;
+
 
     @Override
     public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+
+
+        scheduledTaskRegistrar.setTaskScheduler(poolScheduler());
+
+        scheduledTaskRegistrar.addFixedDelayTask(new IntervalTask(() -> System.out.println("Job @ fixed rate by Runnable Thread " + new Date() + ", Thread name is " + Thread.currentThread().getName()),1000));
+    }
+
+    @Bean
+    public TaskScheduler poolScheduler() {
         ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
 
         threadPoolTaskScheduler.setPoolSize(POOL_SIZE);
         threadPoolTaskScheduler.setThreadNamePrefix("my-scheduled-task-pool-");
         threadPoolTaskScheduler.initialize();
-
-        scheduledTaskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
+        return threadPoolTaskScheduler;
     }
+
+
 }
